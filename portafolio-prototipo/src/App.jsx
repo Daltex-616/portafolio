9abee8869bab70677bfd0f2c6d0211db1a4776f1
Federico Zamora <federicozamora616@gmail.com>
2024-09-08 02:28:08 -0300

    se termino la seccion de perfil

--- portafolio-prototipo/src/App.jsx
+++ portafolio-prototipo/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Navbar } from './navbar/Navbar'
+import { Profile } from './pages/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
       <>
       <Navbar/>
+      <Profile/>
       </>
   )
 }
--- portafolio-prototipo/src/navbar/Navbar.jsx
+++ portafolio-prototipo/src/navbar/Navbar.jsx
@@ -1,30 +1,29 @@
-import { useRef } from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "../public/navbar.css";
 
 export const Navbar = () => {
-  const navRef = useRef();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+  const toggleNavbar = () => {
+    setIsNavOpen(!isNavOpen);
   };
+
   return (
-    <>
-      <header>
-        <h3>LOGO</h3>
-        <nav ref={navRef}>
-          <a href="/#">Home</a>
-          <a href="/#">My work</a>
-          <a href="/#">Blog</a>
-          <a href="/#">About me</a>
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-            <FaTimes />
-          </button>
-        </nav>
-        <button className="nav-btn" onClick={showNavbar}>
-          <FaBars />
+    <header>
+      <h3>DALTEX</h3>
+      <nav className={isNavOpen ? "responsive_nav" : "nav__nav"}>
+        <a href="/#">Home</a>
+        <a href="/#">portafolio</a>
+        <a href="/#">skills</a>
+        <a href="/#">contacto</a>
+        <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
+          <FaTimes />
         </button>
-      </header>
-    </>
+      </nav>
+      <button className="nav-btn" onClick={toggleNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
 };
