5cfcafc494b0c4372633176da58e1fbf9282c40a
Federico Zamora <federicozamora616@gmail.com>
2024-09-07 22:23:35 -0300

    se movio el open menu a un archivo individual

--- portafolio-prototipo/src/navbar/Navbar.jsx
+++ portafolio-prototipo/src/navbar/Navbar.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
+import { OpenMenu } from './OpenMenu'
 export const Navbar = () => { 
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleMenu = () =>{
-            setIsOpen(!isOpen)
-        }
+   
     
   return (
     <div>
@@ -12,19 +10,7 @@ export const Navbar = () => {
             <div>
                 <img src="https://pbs.twimg.com/profile_images/1717245025505914880/hmTsV_yO_400x400.jpg" alt="" />
             </div>
-            <div>
-                <button onClick={toggleMenu}>{isOpen ? "X" : "="}</button>
-                {isOpen && (
-                <nav>
-                    <ul>
-                        <li><a href="#home">Inicio</a></li>
-                        <li><a href="#about">Profile</a></li>
-                        <li><a href="#services">Portafolio</a></li>
-                        <li><a href="#contact">Contacto</a></li>
-                    </ul>
-                </nav>
-            )}
-            </div>
+            <OpenMenu/>
            
         </header>
         
---
+++ portafolio-prototipo/src/navbar/OpenMenu.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export const OpenMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <button onClick={toggleMenu}>
+        {isOpen ? "X" : "="}
+      </button>
+      {isOpen && (
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
+  );
+};
