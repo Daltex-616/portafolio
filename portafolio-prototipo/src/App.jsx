ca63e7d25350d7e781841f5eb8c82eba143c2ca9
Federico Zamora <federicozamora616@gmail.com>
2024-09-07 21:49:25 -0300

    se creo los archivos para la extructura del proyecto y se soluciono un error en main.jsx por la falta de un archivo importado

--- portafolio-prototipo/src/App.jsx
+++ portafolio-prototipo/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import { Navbar } from './navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <>
+      <Navbar/>
       </>
   )
 }
