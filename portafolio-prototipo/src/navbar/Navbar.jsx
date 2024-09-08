d5c16ad1685c1d5623f1da060f9c2f7643390b46
Federico Zamora <federicozamora616@gmail.com>
2024-09-07 22:12:12 -0300

    se creo una barra de navegacion

--- portafolio-prototipo/src/navbar/Navbar.jsx
+++ portafolio-prototipo/src/navbar/Navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
-
-export const Navbar = () => {
+import { useState } from 'react'
+export const Navbar = () => { 
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () =>{
+            setIsOpen(!isOpen)
+        }
+    
   return (
-    <div>Navbar</div>
+    <div>
+        <header className='header'>
+            <div>
+                <img src="https://pbs.twimg.com/profile_images/1717245025505914880/hmTsV_yO_400x400.jpg" alt="" />
+            </div>
+            <div>
+                <button onClick={toggleMenu}>{isOpen ? "X" : "="}</button>
+                {isOpen && (
+                <nav>
+                    <ul>
+                        <li><a href="#home">Inicio</a></li>
+                        <li><a href="#about">Profile</a></li>
+                        <li><a href="#services">Portafolio</a></li>
+                        <li><a href="#contact">Contacto</a></li>
+                    </ul>
+                </nav>
+            )}
+            </div>
+           
+        </header>
+        
+    </div>
   )
 }
